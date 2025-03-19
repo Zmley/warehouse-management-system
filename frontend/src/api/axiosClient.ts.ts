6615356bdbe32a3ac27566c9778916987e70df17
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-03-19 00:08:43 -0300

    改/api 到 env， 以及改名为REACT_APP  开头

--- frontend/src/api/axiosClient.ts.ts
+++ frontend/src/api/axiosClient.ts.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAccessToken } from "../utils/Storages";
 
-const API_BASE_URL = process.env.SERVER_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = process.env.REACT_APP_SERVER_API_BASE_URL || "http://localhost:5001/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
