7c21e6b88f27bc6a3c633c147c6971bd17ed9090
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-03-31 17:03:31 -0300

    refactor ：delete comment

--- frontend/src/pages/Picker/CreateTask.tsx
+++ frontend/src/pages/Picker/CreateTask.tsx
@@ -65,7 +65,7 @@ const CreateTaskPage = () => {
       return
     }
 
-    await createTask(bin.binCode, productCode) // 调用 hook 中的函数来处理任务创建
+    await createTask(bin.binCode, productCode)
     if (!error) {
       navigate('/success')
     } else {
