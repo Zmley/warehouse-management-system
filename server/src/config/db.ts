ddc33db837f955b9752fa8f31561d7d3e9dcdc16
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-03 16:31:52 -0300

    feat： connected datebase

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,8 +2,15 @@ import { Sequelize } from 'sequelize'
 import env from 'config/config'
 
 export const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPassword, {
+  host: env.dbHost,
+  port: 5432,
   dialect: 'postgres',
-  logging: false
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 sequelize
