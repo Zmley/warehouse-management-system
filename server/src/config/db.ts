a199f4b8c0eb098024de8ed4e5dab8f7819fd14d
bowenli0214 <renmendou@gmail.com>
2025-04-03 15:49:17 -0300

    fix: Fix remote issues

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,9 +3,8 @@ import env from './config'
 
 const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPassword, {
   host: env.dbHost,
-  port: env.port || 5432,
+  port: 5432,
   dialect: 'postgres',
-  logging: false,
   dialectOptions: {
     ssl: {
       require: true,
