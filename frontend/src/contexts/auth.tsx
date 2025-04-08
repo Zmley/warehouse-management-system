819ba7a5443f5c83bb058adf5413a06b4f2f7da5
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-07 23:42:37 -0300

    fix： fix token expire bug

--- frontend/src/contexts/auth.tsx
+++ frontend/src/contexts/auth.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react'
-import { areTokensValid } from '../utils/Storages'
+import { areTokensValid, clearTokens } from '../utils/Storages'
 import { getUserProfile } from '../api/authApi'
 import { Task } from '../types/task'
 
@@ -35,8 +35,18 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   )
 
   const getMe = async () => {
-    const account = await getUserProfile()
-    setUserProfile(account)
+    try {
+      const account = await getUserProfile()
+      setUserProfile(account)
+    } catch (error: any) {
+      const status = error?.response?.status
+
+      if (status === 403) {
+        setIsAuthenticated(false)
+        clearTokens()
+        return
+      }
+    }
   }
   return (
     <AuthContext.Provider
