35bd515c41bf1741d8e1c9e3fa66610a14fe48c8
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-04-08 23:48:23 -0300

    chore: format

--- server/src/routes/warehouses/warehouse.controller.ts
+++ server/src/routes/warehouses/warehouse.controller.ts
@@ -1,58 +1,58 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 import {
   getAllWarehouses,
   getWarehouseById,
   createWarehouse,
   updateWarehouse,
-  deleteWarehouse,
-} from "./warehouse.service";
+  deleteWarehouse
+} from './warehouse.service'
 
 export const getAllWarehousesHandler = async (req: Request, res: Response) => {
   try {
-    const warehouses = await getAllWarehouses();
-    res.status(200).json(warehouses);
+    const warehouses = await getAllWarehouses()
+    res.status(200).json(warehouses)
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 export const getWarehouseByIdHandler = async (req: Request, res: Response) => {
-  const { warehouseID } = req.params;
+  const { warehouseID } = req.params
   try {
-    const warehouse = await getWarehouseById(warehouseID);
-    res.status(200).json(warehouse);
+    const warehouse = await getWarehouseById(warehouseID)
+    res.status(200).json(warehouse)
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 export const createWarehouseHandler = async (req: Request, res: Response) => {
-  const { warehouseCode } = req.body;
+  const { warehouseCode } = req.body
   try {
-    const newWarehouse = await createWarehouse(warehouseCode);
-    res.status(201).json(newWarehouse);
+    const newWarehouse = await createWarehouse(warehouseCode)
+    res.status(201).json(newWarehouse)
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 export const updateWarehouseHandler = async (req: Request, res: Response) => {
-  const { warehouseID } = req.params;
-  const { warehouseCode } = req.body;
+  const { warehouseID } = req.params
+  const { warehouseCode } = req.body
   try {
-    const updatedWarehouse = await updateWarehouse(warehouseID, warehouseCode);
-    res.status(200).json(updatedWarehouse);
+    const updatedWarehouse = await updateWarehouse(warehouseID, warehouseCode)
+    res.status(200).json(updatedWarehouse)
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 export const deleteWarehouseHandler = async (req: Request, res: Response) => {
-  const { warehouseID } = req.params;
+  const { warehouseID } = req.params
   try {
-    const message = await deleteWarehouse(warehouseID);
-    res.status(200).json({ message });
+    const message = await deleteWarehouse(warehouseID)
+    res.status(200).json({ message })
   } catch (error: any) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
