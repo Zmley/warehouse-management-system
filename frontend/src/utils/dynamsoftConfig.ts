c887eed2c2fe3882b6d1579bb4b246b9897a42da
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-06-23 19:12:22 -0300

    fix：fix issue

--- frontend/src/utils/dynamsoftConfig.ts
+++ frontend/src/utils/dynamsoftConfig.ts
@@ -1,5 +1,5 @@
 export const dynamsoftConfig = {
-  license: process.env.REACT_APP_DYNAMSOFT_LICENSE!,
+  license: process.env.REACT_APP_DYNAMSOFT_LICENSE,
   container: '.barcode-scanner-view',
   uiPath:
     'https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader-bundle@10.5.3000/dist/',
