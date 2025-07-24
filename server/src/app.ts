223763a16b19e0b370ae20442eb7e481ca6b15ab
Andyzhang222 <Andy.zhang.z@outlook.com>
2025-07-24 13:27:36 -0300

    feat: add CROS domain

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,8 @@ const corsOrigins = [
   process.env.CORS_ORIGIN_ADMIN_PROD,
   process.env.CORS_ORIGIN_ADMIN_PROD_ZMLEY,
   process.env.CORS_ORIGIN_WORKER_LOCAL,
-  process.env.CORS_ORIGIN_WORKER_PROD
+  process.env.CORS_ORIGIN_WORKER_PROD,
+  process.env.CORS_ORIGIN_WORKER_PROD_ZMLEY
 ].filter(Boolean)
 
 const corsOptions = {
